eb73249eb4d036d83907638bc54efb80740bba9a
mada <madaagarcia77@gmail.com>
2022-09-13 09:45:20 +0200

    #1 card detail artistas

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import HeaderRight from './components/Header'
 import Register from './pages/Register'
 import './App.css';
+import Detail from './pages/Detail'
 
 function App() {
  
  
   return (
     <>
-   <Register/>
+   {/* <Register/> */}
+   <Detail/>
   {/* <HeaderRight/>*/}
     </>
   );
