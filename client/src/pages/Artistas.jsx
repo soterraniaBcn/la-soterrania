7934a8026fef9a47dba66bc211de23a02c6d882f
RosiB <blancorosi98@gmail.com>
2022-09-16 12:23:31 +0200

    #17 creating artist list

--- client/src/pages/Artistas.jsx
+++ client/src/pages/Artistas.jsx
@@ -1,13 +1,11 @@
 import HeaderSearch from "../components/HeaderSearch";
 import MediaCard from "../components/MediaCard"
-import Footer from "../components/FooterMui"
 
 export default function Artistas (){
     return(
         <>
         <HeaderSearch/>
         <MediaCard/>
-        <Footer/>
         </>
     )
 }
