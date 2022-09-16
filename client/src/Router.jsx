0ff6df2f5daaf3ea6fd5f863215b44db8979d14e
mada <madaagarcia77@gmail.com>
2022-09-16 14:06:15 +0200

    #1 create card detail event

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -7,6 +7,7 @@ import HomePage from './components/HomePage'
 import DetailArtista from './pages/DetailArtista'
 import DetailSpace from './pages/DetailSpace'
 import PerfilAdmin from './pages/PerfilAdmin'
+import DetailEvent from './pages/DetailEvent'
 
 
 export default function Router() {
@@ -19,6 +20,7 @@ export default function Router() {
         <Route exact path="/homepage" element={<HomePage />} />
         <Route exact path="/artist/detail" element={<DetailArtista />} />
         <Route exact path="/space/detail" element={<DetailSpace />} />
+        <Route exact path="/events" element={<DetailEvent />} />
         <Route exact path="/perfiladmi" element={<PerfilAdmin />} />
       </Routes>
     </div>
