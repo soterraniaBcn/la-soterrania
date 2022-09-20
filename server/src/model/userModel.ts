d91f451d6095b9ccc79f1478060dddefe8803df7
Belén <104005148+Nedeltoga@users.noreply.github.com>
2022-09-20 15:18:35 +0200

    #14 change database name in queries

--- server/src/model/userModel.ts
+++ server/src/model/userModel.ts
@@ -4,7 +4,7 @@ import { iUser, iUserLogin } from "./interface/iUser";
 class User {
   async saveUser(users: iUser) {
     const queryStr =
-      "INSERT INTO publicusuaria (estado, email1, password, rol) VALUES ($1, $2, $3, $4) RETURNING *";
+      "INSERT INTO public.usuaria (estado, email1, password, rol) VALUES ($1, $2, $3, $4) RETURNING *";
     const values = [
       users.estado,
       users.email1,
@@ -16,27 +16,27 @@ class User {
   }
   async getUserById(id_usuaria: any) {
     const queryStr =
-      'SELECT "id-usuaria", estado, email1, rol FROM publicusuaria WHERE "id-usuaria"= $1';
+      'SELECT "id-usuaria", estado, email1, rol FROM public.usuaria WHERE "id-usuaria"= $1';
     const result: any = await connection.query(queryStr, [id_usuaria]);
     console.dir(result);
     return result.rows[0];
   }
   async getUserByEmail(email1: any) {
-    const queryStr = "SELECT * FROM publicusuaria WHERE email1= $1";
+    const queryStr = "SELECT * FROM public.usuaria WHERE email1= $1";
     const values = [email1];
     const result: any = await connection.query(queryStr, values);
     return result.rows[0];
   }
   async getAllUsers() {
     const queryStr =
-      'SELECT "id-usuaria", estado, email1, rol FROM publicusuaria';
+      'SELECT "id-usuaria", estado, email1, rol FROM public.usuaria';
     const result: any = await connection.query(queryStr, []);
     console.dir(result);
     return result.rows;
   }
   async deleteUser(id_usuaria: any) {
     const queryStr =
-      "DELETE FROM publicusuaria WHERE 'id-usuaria'=$1 RETURNING *";
+      "DELETE FROM public.usuaria WHERE 'id-usuaria'=$1 RETURNING *";
     const result: any = await connection.query(queryStr, [id_usuaria]);
     return result.rows;
   }
