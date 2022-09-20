826db06201aad7321f851f41a53966366674f571
Belén <104005148+Nedeltoga@users.noreply.github.com>
2022-09-20 15:09:31 +0200

    #14 remove build folder

--- server/build/controllers/loginController.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var loginController = function (req, res) {
-    res.json({ token: req.body.token, rol: req.body.rol });
-};
-exports["default"] = loginController;
