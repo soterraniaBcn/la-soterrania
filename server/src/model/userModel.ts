5fab185fc76f1a66aa94386e3973b51487170fe1
Belén <104005148+Nedeltoga@users.noreply.github.com>
2022-09-20 15:15:49 +0200

    #14 fix queries from postgress sql

--- server/src/model/userModel.ts
+++ server/src/model/userModel.ts
@@ -4,7 +4,7 @@ import { iUser, iUserLogin } from "./interface/iUser";
 class User {
   async saveUser(users: iUser) {
     const queryStr =
-      "INSERT INTO soterraniatestschema.usuarias (estado, email1, password, rol) VALUES ($1, $2, $3, $4) RETURNING *";
+      "INSERT INTO publicusuaria (estado, email1, password, rol) VALUES ($1, $2, $3, $4) RETURNING *";
     const values = [
       users.estado,
       users.email1,
@@ -16,27 +16,27 @@ class User {
   }
   async getUserById(id_usuaria: any) {
     const queryStr =
-      'SELECT "id-usuaria", estado, email1, rol FROM soterraniatestschema.usuarias WHERE "id-usuaria"= $1';
+      'SELECT "id-usuaria", estado, email1, rol FROM publicusuaria WHERE "id-usuaria"= $1';
     const result: any = await connection.query(queryStr, [id_usuaria]);
     console.dir(result);
     return result.rows[0];
   }
   async getUserByEmail(email1: any) {
-    const queryStr =
-      "SELECT * FROM soterraniatestschema.usuarias WHERE email1= $1";
+    const queryStr = "SELECT * FROM publicusuaria WHERE email1= $1";
     const values = [email1];
     const result: any = await connection.query(queryStr, values);
     return result.rows[0];
   }
   async getAllUsers() {
     const queryStr =
-      'SELECT "id-usuaria", estado, email1, rol FROM soterraniatestschema.usuarias';
+      'SELECT "id-usuaria", estado, email1, rol FROM publicusuaria';
     const result: any = await connection.query(queryStr, []);
     console.dir(result);
     return result.rows;
   }
   async deleteUser(id_usuaria: any) {
-    const queryStr = "DELETE FROM soterraniatestschema.usuarias WHERE 'id-usuaria'=$1 RETURNING *";
+    const queryStr =
+      "DELETE FROM publicusuaria WHERE 'id-usuaria'=$1 RETURNING *";
     const result: any = await connection.query(queryStr, [id_usuaria]);
     return result.rows;
   }
