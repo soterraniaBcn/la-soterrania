fb52cd94cb0ab74d442224900db69565afa4759f
RosiB <blancorosi98@gmail.com>
2022-09-20 16:11:58 +0200

    llamada a la api falsa creada, solo falta editar el header

--- client/src/components/MediaCard.jsx
+++ client/src/components/MediaCard.jsx
@@ -1,20 +1,41 @@
 import React, { useState, useEffect } from "react";
-//import axios from 'axios';
-//import { useSearchParams, Link } from 'react-router-dom';
+import axios from 'axios';
+import { useSearchParams, Link } from 'react-router-dom';
 import '../css/MediaCard.css'
 
 export default function MediaCard() {
+  const [searchParams, setSearchParams]= useSearchParams ()
+  const search= searchParams.get('search') || ''
+  const [card, setCard] = useState([]);
+  useEffect(() => {
+    axios({
+      url: "http://localhost:3000/artistas",
+    })
+      .then((response) => {
+        console.log(response)
+        const filterArray=response.data.filter(element=>{
+          if (search!='')
+                   return element.nombre.toLowerCase().includes(search.toLocaleLowerCase())
+         return element })
+        setCard(filterArray)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [search]);
   return (
-
+    <div className="div-cards">
+  {card.map(item =>(
   <article className="flow">
      <div className="team">
-        <a href="google.com" className="profile">
-          <h2 className="profile__name">Nombre de la artista</h2>
-          <p>Genero</p>
-          <img alt="Anita Simmons" src="https://source.unsplash.com/BhcutpohYwg/800x800" />
+        <a href={item.link} className="profile">
+          <h2 className="profile__name">{item.nombre}</h2>
+          <p>{item.genero}</p>
+          <img alt="Anita Simmons" src={item.img} />
         </a>
     </div>
  </article>
-    
+    ))}
+    </div>
   );
 }
