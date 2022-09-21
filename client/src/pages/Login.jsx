bb23e8cf824dd23d95920dc7aab88bcc6085507d
Belén <104005148+Nedeltoga@users.noreply.github.com>
2022-09-21 14:58:36 +0200

    #14 merge login solved

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,36 +1,16 @@
-
-import React from "react";
+import React, { useState } from "react";
 import { Grid, FormGroup, Box, Button } from "@mui/material";
 import Header from "../components/Header";
-import {Link} from 'react-router-dom'
+import { Link, useNavigate } from "react-router-dom";
+import { authService } from "../services/authService";
 
-
-
-<<<<<<< HEAD
 export default function Login() {
-  return (
-    <>
-      <Grid
-        container
-        style={{
-          backgroundColor: '#E2A0FF',
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexDirection: 'column',
-          marginTop: 0,
-        }}
-      >
-        <Header />
-=======
-export default function Login(){
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [user, setUser] = useState({ email, password });
   const navigation = useNavigate();
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const user = { email: email, password: password };
@@ -40,120 +20,103 @@ export default function Login(){
         setUser(res.data.token);
         localStorage.setItem("user", res.data.token);
         if (res.data.token) {
-          navigation("/myprofile", { replace: true })
+          navigation("/myprofile", { replace: true });
         }
       })
       .catch(() => setError("Hubo un error"));
   };
->>>>>>> login
-
+  return (
+    <>
+      <Grid
+        container
+        style={{
+          backgroundColor: "#E2A0FF",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          marginTop: 0,
+        }}
+      >
+        <Header />
         <Grid
           item
           xs={8}
           style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: 'white',
-            width: '25rem',
-            maxHeight: '30rem',
-            borderRadius: '1rem',
-            border: '5px solid #cdcecf',
-            marginTop: '6rem',
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "white",
+            width: "25rem",
+            maxHeight: "30rem",
+            borderRadius: "1rem",
+            border: "5px solid #cdcecf",
+            marginTop: "6rem",
           }}
         >
           <FormGroup className="form" xs={8}>
             <p
               style={{
-                fontSize: '2rem',
-                display: 'flex',
-                justifyContent: 'center',
+                fontSize: "2rem",
+                display: "flex",
+                justifyContent: "center",
               }}
             >
               LOGIN
             </p>
-            <Box style={{ lineHeight: '1px' }}>
+            <Box style={{ lineHeight: "1px" }}>
               <p>Correu electrònic:</p>
               <input
                 type="text"
                 style={{
-                  fontSize: '1.2rem',
-                  borderRadius: '10px',
-                  border: '1px solid grey',
+                  fontSize: "1.2rem",
+                  borderRadius: "10px",
+                  border: "1px solid grey",
                 }}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </Box>
-
-
-
             <Box sx={{ mt: "3rem" }} style={{ lineHeight: "1px" }}>
-
               <p>Contrasenya:</p>
               <input
                 type="text"
                 style={{
-                  fontSize: '1.2rem',
-                  borderRadius: '10px',
-                  border: '1px solid grey',
+                  fontSize: "1.2rem",
+                  borderRadius: "10px",
+                  border: "1px solid grey",
                 }}
+                onChange={(e) => setPassword(e.target.value)} 
               />
             </Box>
-            <Box style={{ lineHeight: '1px', fontSize: '0.8rem' }}>
+            <Box style={{ lineHeight: "1px", fontSize: "0.8rem" }}>
               <p>Encara no tens compte? </p>
-
-<<<<<<< HEAD
               <p>Pots enregistrar-te</p>
               <a href="/register">aquí</a>
             </Box>
           </FormGroup>
         </Grid>
-
-   <Link to='/homepage'>
-        <Button
-          style={{
-            backgroundColor: "#cdcecf",
-            width: "7rem",
-            height: "2rem",
-            borderRadius: "40px",
-            color: "black",
-            textTransform: "capitalize",
-            fontSize: "1rem",
-            fontWeight: "200",
-            border: "2.5px solid grey",
-            margin: "1.3em",
-          }}
-        >
-          Enviar{" "}
-        </Button>
-
+        <Link to="/homepage">
+          <Button
+            onClick={handleSubmit}
+            style={{
+              backgroundColor: "#cdcecf",
+              width: "7rem",
+              height: "2rem",
+              borderRadius: "40px",
+              color: "black",
+              textTransform: "capitalize",
+              fontSize: "1rem",
+              fontWeight: "200",
+              border: "2.5px solid grey",
+              margin: "1.3em",
+            }}
+          >
+            Enviar{" "}
+          </Button>
         </Link>
       </Grid>
+      {error}
     </>
-  )
-=======
-
-          </FormGroup> 
-          </Grid>
-                <Button
-                  style={{
-                      backgroundColor: "#cdcecf",
-                      width: "7rem",
-                      height: "2rem",
-                      borderRadius: "40px",
-                      color: "black",
-                      textTransform: "capitalize",
-                      fontSize: "1rem",
-                      fontWeight: "200",
-                      border: "2.5px solid grey",
-                      margin: "1.3em"
-                    }}
-                    onClick={handleSubmit}
-                    >
-                  Enviar </Button>
-        </Grid>
-        {error}
-        </>
-       
-    )
->>>>>>> login
+  );
 }
