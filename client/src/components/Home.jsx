cdef84a727a73b2156b1bac06e524e2244f07827
hilla-sb <hillsb2302@gmail.com>
2022-09-22 01:34:49 +0200

    #1 cambios de style de homepage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,9 @@
-import background from '../imagenes/background.png';
-import { Grid, Container, Button } from "@mui/material";
+import background5 from '../imagenes/background5.png';
+import { Grid, Container, Button,Box } from "@mui/material";
 import React from 'react';
 import { Link } from "react-router-dom";
+import Menu from "./Menu.jsx";
+import Header from "./Header.jsx";
 
 
 
@@ -9,23 +11,25 @@ import { Link } from "react-router-dom";
 export default function Home(){
     return(
      
-       <Container fluid >
-       <Grid container xl={12} >
-     
-        
-                <img className="hero-image" src={background} alt=""/>
+   
+       <Grid container className="padre2" style={{}}>
+       <Header/>
+        <Menu/>
     
-                <h1 className ="hero-text">La soterrània</h1>
-                <h6 className ="hero-text">is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</h6>
-                
-                <Link to="/login" style={{textUnderline: "none"}}> 
-                <Button className="btn-grad">Enviar</Button>
+                <img className="hero-image" src={background5} alt=""/>
+    
+                <h1 className ="hero-text">la soterrània</h1>
+                <p className ="hero-texts">Plataforma de difusió cultural que promou vincles entre artistes i espais socials/culturals alternatius.</p>
+                <Box style ={{width:"100%"}}>
+                <Link className ="button1" to="/login" style={{textUnderline: "none"}}> 
+                <Button className="btn-grad">Login</Button>
                 </Link>
-               
+                </Box>
 
             </Grid>
+            
 
-        </Container>
+       
 
 
 
