8e41f4606e9682c1b2bb24740bf26eab68a31f8c
Belén <104005148+Nedeltoga@users.noreply.github.com>
2022-09-22 15:52:55 +0200

    #1 MVC getAllArtists and saveArtist started

--- server/src/model/artistsModel.tsx
+++ server/src/model/artistsModel.tsx
@@ -1,19 +1,37 @@
 import connection from "../services/database.service";
+import { iArtist } from "./interface/iArtist";
 
-//revisar
-import { iUser, iUserLogin } from "./interface/iUser";
-
-class User {
-  async saveUser(users: iUser) {
+class Artist {
+  async saveArtist(artists: iArtist) {
     const queryStr =
-      "INSERT INTO public.usuarias (estado, email, password, rol) VALUES ($1, $2, $3, $4) RETURNING *";
+      "INSERT INTO public.artista (id, estado, nombre, foto, genero, descripcion, instrumentacion, telefono1, telefono2, email, web, redsocial1, redsocial2, redsocial3, notas, id_usuaria, id_ciudad) VALUES ($1, $2, $3, $4, $5 $6 $7 $8 $9 $10 $11 $12 $13 $14 $15 $16 $17) RETURNING *";
     const values = [
-      users.estado,
-      users.email,
-      users.password,
-      users.rol,
+      artists.id,
+      artists.estado,
+      artists.nombre,
+      artists.foto,
+      artists.genero,
+      artists.descripcion,
+      artists.instrumentacion,
+      artists.telefono1,
+      artists.telefono2,
+      artists.email,
+      artists.web,
+      artists.redsocial1,
+      artists.redsocial2,
+      artists.redsocial3,
+      artists.notas,
+      artists.id_usuaria,
+      artists.id_ciudad,
     ] as string[];
     const result: any = await connection.query(queryStr, values);
     return result.rows[0];
   }
+
+  async getAllArtists() {
+    const queryStr = 'SELECT "id", estado, nombre, foto, genero, descripcion, instrumentacion, telefono1, telefono2, email, web, redsocial1, redsocial2, redsocial3, notas, id_usuaria, id_ciudad FROM public.artista';
+    const result: any = await connection.query(queryStr, []);
+    return result.rows;
+  }
+  
 }
---
+++ server/src/routes/artistsRoute.ts
@@ -0,0 +1,13 @@
+import Router from 'express';
+import artistsController from '../controllers/artistsController';
+import auth from '../middlewares/auth';
+import jwt from '../middlewares/jwt';
+import state from '../middlewares/state';
+import { encryptPassword } from "../middlewares/auth";
+
+const router = Router();
+
+router.get('/artistas', auth.validateUser, state.checkState, artistsController);
+router.post("/artistas", encryptPassword, artistsController.saveArtist);
+
+export default router; 
