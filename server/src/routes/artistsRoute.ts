dc2b64188268fccd47c028087e2b6698f6a54c92
RosiB <blancorosi98@gmail.com>
2022-09-27 12:56:24 +0200

    #17 cambio de rutas de artists

--- server/src/routes/artistsRoute.ts
+++ server/src/routes/artistsRoute.ts
@@ -5,11 +5,11 @@ import admin from '../middlewares/admin';
 
 const router = Router();
 
-router.get('/artistas', jwt.validateToken, artistsController.getAllArtists);
-router.get('/artistas/:id', jwt.validateToken, artistsController.getOneArtist);
-router.post("/artistas", jwt.validateToken, admin.checkRol, artistsController.saveArtist);
-router.put('/artistas/:id', jwt.validateToken, admin.checkRol, artistsController.modifyArtist)
-router.delete('/artistas/:id', jwt.validateToken, admin.checkRol, artistsController.deleteArtist)
+router.get('/artistes', jwt.validateToken, artistsController.getAllArtists);
+router.get('/artistes/:id', jwt.validateToken, artistsController.getOneArtist);
+router.post("/artistes", jwt.validateToken, admin.checkRol, artistsController.saveArtist);
+router.put('/artistes/:id', jwt.validateToken, admin.checkRol, artistsController.modifyArtist)
+router.delete('/artistes/:id', jwt.validateToken, admin.checkRol, artistsController.deleteArtist)
 
 
 export default router; 
