cf85c409f6b24242e15f8d8998ed0ad918cf15d5
RosiB <blancorosi98@gmail.com>
2022-09-27 14:10:14 +0200

    agregacion del bearer token a la ruta de artistas

--- client/src/services/getArtists.jsx
+++ client/src/services/getArtists.jsx
@@ -2,7 +2,8 @@ import axios from "axios"
 
 const GetArtists = {   
     async getAll(){ 
-       const result = await axios.get (process.env.REACT_APP_BASE_URL+"/artistes")
+       const result = await axios.get (process.env.REACT_APP_BASE_URL+"/artistes", { headers: { authorization: `Bearer ${localStorage.getItem("user")}` } }
+       )
        return result.data    
     }
 } 
