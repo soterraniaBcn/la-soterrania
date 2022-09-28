1cf5ffaf233a5a381459aebe4e160e97b5fe626b
Belén <104005148+Nedeltoga@users.noreply.github.com>
2022-09-28 15:15:03 +0200

    #11 MVC deleteEvent working in back

--- server/src/model/eventModel.ts
+++ server/src/model/eventModel.ts
@@ -7,6 +7,11 @@ class Event {
         console.dir(result);
         return result.rows;
       }
+      async getOneEvent(id: any){
+        const queryStr = 'SELECT * FROM public.evento WHERE id=$1;'
+        const result:any = await connection.query(queryStr,[id])
+        return result.rows[0];
+    }
       async saveEvent(events: iEvent) {
         const queryStr =
           "INSERT INTO public.evento ( estado, nombre, foto,descripcion, ubicacion, organizacion, telefono1, telefono2,email1, email2, web, redsocial1, redsocial2, redsocial3,fechainicio, horainicio, fechafin, horafin) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18 ) RETURNING *";
@@ -33,6 +38,34 @@ class Event {
         const result: any = await connection.query(queryStr, values);
         return result.rows[0];
       }
+      async modifyEvent(event: iEvent){
+        const queryStr='UPDATE evento SET (estado, nombre, foto,descripcion, ubicacion, organizacion, telefono1, telefono2,email1, email2, web, redsocial1, redsocial2, redsocial3,fechainicio, horainicio, fechafin, horafin) = ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18)  WHERE id = $19 RETURNING *';
+        const result:any = await connection.query(queryStr,[event.estado,
+          event.nombre,
+          event.foto,
+          event.descripcion,
+          event.ubicacion,
+          event.organizacion, 
+          event.telefono1 || null,
+          event.telefono2 || null,
+          event.email1,
+          event.email2 || null,
+          event.web,
+          event.redsocial1 || null,
+          event.redsocial2 || null,
+          event.redsocial3 || null,
+          event.fechainicio,
+          event.horainicio,
+          event.fechafin,
+          event.horafin,
+          event.id]);
+        return result.rows;
+      }
+      async deleteEvent(id:any){
+        const queryStr = 'DELETE FROM public.artista WHERE id=$1 RETURNING *';
+        const result:any= await connection.query(queryStr, [id]);
+        return result.rows;
+    }
 }
 
 export default new Event();
--- server/src/routes/eventRoute.ts
+++ server/src/routes/eventRoute.ts
@@ -6,11 +6,24 @@ import admin from "../middlewares/admin";
 const router = Router();
 
 router.get("/events", jwt.validateToken, eventController.getAllEvents);
+router.get("/events/:id", jwt.validateToken, eventController.getOneEvent);
 router.post(
     "/events",
     jwt.validateToken,
     admin.checkRol,
     eventController.saveEvent
+  );
+  router.put(
+    "/events/:id",
+    jwt.validateToken,
+    admin.checkRol,
+    eventController.modifyEvent
+  );
+  router.delete(
+    "/events/:id",
+    jwt.validateToken,
+    admin.checkRol,
+    eventController.deleteEvent
   );
 
 export default router;
